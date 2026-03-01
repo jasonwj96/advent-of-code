b3c20bffdf1e46239ca5124fe35d0abc968a6a27
Jason Wedderburn <jasonwj96@gmail.com>
2026-02-28 19:24:36 -0500

    2025 day 1 part 1 fixes and removed 2024

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
-pub fn parse<P: AsRef<Path>>(path: P) -> io::Result<Vec<(char, u32)>> {
+pub fn parse<P: AsRef<Path>>(path: P) -> io::Result<Vec<(char, i32)>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     let mut result = Vec::new();
@@ -13,7 +13,7 @@ pub fn parse<P: AsRef<Path>>(path: P) -> io::Result<Vec<(char, u32)>> {
 
         let mut chars = line.chars();
         let dir = chars.next().unwrap();
-        let value: u32 = chars.as_str().parse().unwrap();
+        let value: i32 = chars.as_str().parse().unwrap();
 
         result.push((dir, value));
     }
@@ -21,12 +21,12 @@ pub fn parse<P: AsRef<Path>>(path: P) -> io::Result<Vec<(char, u32)>> {
     Ok(result)
 }
 
-pub fn part1(input: &[(char, u32)]) -> u32 {
+
+pub fn part1(input: &[(char, i32)]) -> u32 {
     let mut dial: i32 = 50;
     let mut result = 0;
 
     for &(dir, steps) in input {
-        let steps = steps as i32;
 
         dial += match dir {
             'R' => steps,
@@ -42,22 +42,21 @@ pub fn part1(input: &[(char, u32)]) -> u32 {
     result
 }
 
-pub fn part2(input: &[(char, u32)]) -> i32 {
+
+pub fn part2(input: &[(char, i32)]) -> u32 {
     let mut dial: i32 = 50;
-    let mut result: i32 = 0;
+    let result: u32 = 0;
 
     for &(dir, steps) in input {
-        for _ in 0..steps {
-            match dir {
-                'R' => dial = (dial + 1) % 100,
-                'L' => dial = (dial + 99) % 100,
-                _ => {}
-            }
+        dial += match dir {
+            'R' => steps,
+            'L' => -steps,
+            _ => 0,
+        };
 
-            if dial == 0 {
-                result += 1;
-            }
-        }
+        // Validate lands in 0
+
+        dial = dial.rem_euclid(100);
     }
 
     result
